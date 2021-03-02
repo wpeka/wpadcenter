6744283cee19676070cdca84b460149bd55e2739
Pranay Chahare <pranay.chahare@hbwsl.com>
2021-03-02 15:37:17 +0530

    AD-49:added image file and fixed url for wpeka

--- wpadcenter.php
+++ wpadcenter.php
@@ -17,8 +17,8 @@
  * Plugin URI:        https://wpadcenter.com
  * Description:       This is a short description of what the plugin does. It's displayed in the WordPress admin area.
  * Version:           1.0.0
- * Author:            WPEka
- * Author URI:        https://wpadcenter.com/
+ * Author:            WPEka Club
+ * Author URI:        https://club.wpeka.com/
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
  * Text Domain:       wpadcenter
