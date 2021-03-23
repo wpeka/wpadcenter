4fbb37dc32ee7c75cff067e993762ce88a56b4d0
Pranay Chahare <>
2021-03-23 13:59:46 +0530

    Added validations for elementor widget extension

--- includes/elementor/class-wpadcenter-elementor-widgets.php
+++ includes/elementor/class-wpadcenter-elementor-widgets.php
@@ -9,6 +9,11 @@
  * @subpackage Wpadcenter/includes/elementor
  */
 
+if ( ! defined( 'ABSPATH' ) ) {
+	exit; // Exit if accessed directly.
+}
+
+
 /**
  * Class for registering elementor widgets.
  *
@@ -20,6 +25,24 @@
  * @author     WPEka <hello@wpeka.com>
  */
 class Wpadcenter_Elementor_Widgets {
+
+	/**
+	 * Minimum Elementor Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum Elementor version required to run the plugin.
+	 */
+	const MINIMUM_ELEMENTOR_VERSION = '2.0.0';
+
+	/**
+	 * Minimum PHP Version
+	 *
+	 * @since 1.0.0
+	 *
+	 * @var string Minimum PHP version required to run the plugin.
+	 */
+	const MINIMUM_PHP_VERSION = '7.0';
 
 	/**
 	 * Instance of the class
@@ -51,8 +74,80 @@ class Wpadcenter_Elementor_Widgets {
 	 * @since 1.0.0
 	 */
 	protected function __construct() {
+
+		add_action( 'plugins_loaded', array( $this, 'on_plugins_loaded' ) );
+
+	}
+
+	/**
+	 * On Plugins Loaded
+	 *
+	 * Checks if Elementor has loaded, and performs some compatibility checks.
+	 * If All checks pass, inits the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function on_plugins_loaded() {
+
+		if ( $this->is_compatible() ) {
+			add_action( 'elementor/init', array( $this, 'init' ) );
+		}
+
+	}
+
+	/**
+	 * Compatibility Checks
+	 *
+	 * Checks if the installed version of Elementor meets the plugin's minimum requirement.
+	 * Checks if the installed PHP version meets the plugin's minimum requirement.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function is_compatible() {
+
+		// Check if Elementor installed and activated.
+		if ( ! did_action( 'elementor/loaded' ) ) {
+			return false;
+		}
+
+		// Check for required Elementor version.
+		if ( ! version_compare( ELEMENTOR_VERSION, self::MINIMUM_ELEMENTOR_VERSION, '>=' ) ) {
+			return false;
+		}
+
+		// Check for required PHP version.
+		if ( version_compare( PHP_VERSION, self::MINIMUM_PHP_VERSION, '<' ) ) {
+			return false;
+		}
+
+		return true;
+
+	}
+
+	/**
+	 * Initialize the plugin
+	 *
+	 * Load the plugin only after Elementor (and other plugins) are loaded.
+	 * Load the files required to run the plugin.
+	 *
+	 * Fired by `plugins_loaded` action hook.
+	 *
+	 * @since 1.0.0
+	 *
+	 * @access public
+	 */
+	public function init() {
+
+		// Add Plugin actions.
 		require_once plugin_dir_path( __DIR__ ) . 'elementor/class-wpadcenter-elementor-singlead-widget.php';
 		add_action( 'elementor/widgets/widgets_registered', array( $this, 'register_widgets' ) );
+
 	}
 
 	/**
@@ -66,13 +161,5 @@ class Wpadcenter_Elementor_Widgets {
 
 }
 
-add_action( 'init', 'wpadcenter_elementor_init' );
 
-	/**
-	 * Initializes class
-	 *
-	 * @since 1.0.0
-	 */
-function wpadcenter_elementor_init() {
 	Wpadcenter_Elementor_Widgets::get_instance();
-}
