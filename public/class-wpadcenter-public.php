4849f922ff5263079d45109a2bbb06279d37cd4d
Dhananjay Mayane <dhananjay.mayane@hbwsl.com>
2021-03-30 15:31:20 +0530

    bug fixes, changes (#69) (#71)
    * bug fixes
    
    * bug fix for ads.txt setting impressions
    
    Co-authored-by: premsurana <premx81@gmail.com>
    
    Co-authored-by: Prem Surana <77276337+premsurana-hbwsl@users.noreply.github.com>
    Co-authored-by: premsurana <premx81@gmail.com>

--- public/class-wpadcenter-public.php
+++ public/class-wpadcenter-public.php
@@ -274,7 +274,10 @@ class Wpadcenter_Public {
 	 * @return string $single_ad_html html for the ad to be displayed.
 	 */
 	public static function display_single_ad( $ad_id, $attributes = array() ) {
-
+		$current_url = isset( $_SERVER['REQUEST_URI'] ) ? home_url( sanitize_text_field( wp_unslash( $_SERVER['REQUEST_URI'] ) ) ) : '';
+		if ( strstr( $current_url, 'ads.txt' ) ) {
+			return;
+		}
 		wp_enqueue_style( 'wpadcenter-frontend' );
 		wp_enqueue_script( 'wpadcenter-frontend' );
 		wp_localize_script(
@@ -419,12 +422,14 @@ class Wpadcenter_Public {
 				add_action( 'wp_body_open', array( $this, 'wpadcenter_output_body_post' ) );
 			}
 			add_action( 'wp_footer', array( $this, 'wpadcenter_output_footer_post' ) );
-			if ( 'off' === $disable_global_scripts['disable_global_scripts'] ) {
-				add_action( 'wp_head', array( $this, 'wpadcenter_output_header_global' ) );
-				if ( $body_open_supported ) {
-					add_action( 'wp_body_open', array( $this, 'wpadcenter_output_body_global' ) );
+			if ( is_array( $disable_global_scripts ) ) {
+				if ( 'off' === $disable_global_scripts['disable_global_scripts'] ) {
+					add_action( 'wp_head', array( $this, 'wpadcenter_output_header_global' ) );
+					if ( $body_open_supported ) {
+						add_action( 'wp_body_open', array( $this, 'wpadcenter_output_body_global' ) );
+					}
+					add_action( 'wp_footer', array( $this, 'wpadcenter_output_footer_global' ) );
 				}
-				add_action( 'wp_footer', array( $this, 'wpadcenter_output_footer_global' ) );
 			}
 		}
 
