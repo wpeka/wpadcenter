e4bbe32895f4d40c9c709de276fcf17fe4d6feb3
sonali <sonalirlondhe.01@gmail.com>
2021-04-08 17:08:20 +0530

    AD-19: Multisite compatibility issue Fixed

--- includes/class-wpadcenter-activator.php
+++ includes/class-wpadcenter-activator.php
@@ -19,45 +19,43 @@
  * @subpackage Wpadcenter/includes
  * @author     WPEka <hello@wpeka.com>
  */
-class Wpadcenter_Activator
-{
+class Wpadcenter_Activator {
 
-    /**
-     * Short Description. (use period)
-     *
-     * Long Description.
-     *
-     * @since 1.0.0
-     */
-    public static function activate()
-    {
-        global $wpdb;
-        include_once ABSPATH . 'wp-admin/includes/upgrade.php';
-        if (is_multisite() ) {
-            // Get all blogs in the network and activate plugin on each one
-            $blog_ids = $wpdb->get_col("SELECT blog_id FROM $wpdb->blogs");
-            foreach ( $blog_ids as $blog_id ) {
-                switch_to_blog($blog_id);
-                Wpadcenter_Activator::wpadcenter_install_table();
-                restore_current_blog();
-            }
-        } else {
-            Wpadcenter_Activator::wpadcenter_install_table();
-        }
-    }
 
-    public static function wpadcenter_install_table()
-    {
-        global $wpdb;
-        $charset_collate = $wpdb->get_charset_collate();
-        $table_name = $wpdb->prefix . 'ads_statistics';
-        $sql = "CREATE TABLE $table_name (
+	/**
+	 * Short Description. (use period)
+	 *
+	 * Long Description.
+	 *
+	 * @since 1.0.0
+	 */
+	public static function activate() {
+		global $wpdb;
+		include_once ABSPATH . 'wp-admin/includes/upgrade.php';
+		if ( is_multisite() ) {
+			// Get all blogs in the network and activate plugin on each one
+			$blog_ids = $wpdb->get_col( "SELECT blog_id FROM $wpdb->blogs" );
+			foreach ( $blog_ids as $blog_id ) {
+				switch_to_blog( $blog_id );
+				self::wpadcenter_install_table();
+				restore_current_blog();
+			}
+		} else {
+			self::wpadcenter_install_table();
+		}
+	}
+
+	public static function wpadcenter_install_table() {
+		global $wpdb;
+		$charset_collate = $wpdb->get_charset_collate();
+		$table_name      = $wpdb->prefix . 'ads_statistics';
+		$sql             = "CREATE TABLE $table_name (
 			ad_id int(11) NOT NULL,
 			ad_date DATE DEFAULT NULL,
 			ad_clicks int(11) DEFAULT 0,
 			ad_impressions int(11) DEFAULT 0
 			) $charset_collate;";
-        dbDelta($sql);
-    }
+		dbDelta( $sql );
+	}
 
 }
--- wpadcenter.php
+++ wpadcenter.php
@@ -25,77 +25,76 @@
  */
 
 // If this file is called directly, abort.
-if (! defined('WPINC') ) {
-    die;
+if ( ! defined( 'WPINC' ) ) {
+	die;
 }
 
-if (! defined('WPADCENTER_PLUGIN_URL') ) {
-    define('WPADCENTER_PLUGIN_URL', plugin_dir_url(__FILE__));
+if ( ! defined( 'WPADCENTER_PLUGIN_URL' ) ) {
+	define( 'WPADCENTER_PLUGIN_URL', plugin_dir_url( __FILE__ ) );
 }
 
-if (! defined('WPADCENTER_SCRIPT_SUFFIX') ) {
-    define('WPADCENTER_SCRIPT_SUFFIX', ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) ? '' : '.min');
+if ( ! defined( 'WPADCENTER_SCRIPT_SUFFIX' ) ) {
+	define( 'WPADCENTER_SCRIPT_SUFFIX', ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min' );
 }
 
-if (! defined('WPADCENTER_PLUGIN_FILENAME') ) {
-    define('WPADCENTER_PLUGIN_FILENAME', __FILE__);
+if ( ! defined( 'WPADCENTER_PLUGIN_FILENAME' ) ) {
+	define( 'WPADCENTER_PLUGIN_FILENAME', __FILE__ );
 }
 
-define('WPADCENTER_SETTINGS_FIELD', 'WPAdCenter-Settings');
+define( 'WPADCENTER_SETTINGS_FIELD', 'WPAdCenter-Settings' );
 
 /**
  * Currently plugin version.
  * Start at version 1.0.0 and use SemVer - https://semver.org
  * Rename this for your plugin and update it as you release new versions.
  */
-define('WPADCENTER_VERSION', '2.0.0');
+define( 'WPADCENTER_VERSION', '2.0.0' );
 
-if (! function_exists('adc_fs') ) {
-    /**
-     * Helper function to access SDK.
-     *
-     * @return Analytics
-     */
-    function adc_fs()
-    {
-        global $adc_fs;
+if ( ! function_exists( 'adc_fs' ) ) {
+	/**
+	 * Helper function to access SDK.
+	 *
+	 * @return Analytics
+	 */
+	function adc_fs() {
+		global $adc_fs;
 
-        if (! isset($adc_fs) ) {
-            // Include Analytics SDK.
-            include_once dirname(__FILE__) . '/analytics/start.php';
+		if ( ! isset( $adc_fs ) ) {
+			// Include Analytics SDK.
+			include_once dirname( __FILE__ ) . '/analytics/start.php';
 
-            $adc_fs = ras_dynamic_init(
-                array(
-                'id'              => '10',
-                'slug'            => 'wpadcenter',
-                'product_name'    => 'WPAdCenter',
-                'module_type'     => 'plugin',
-                'version'         => WPADCENTER_VERSION,
-                'plugin_basename' => 'wpadcenter/wpadcenter.php',
-                'plugin_url'      => WPADCENTER_PLUGIN_URL,
-                )
-            );
-        }
+			$adc_fs = ras_dynamic_init(
+				array(
+					'id'              => '10',
+					'slug'            => 'wpadcenter',
+					'product_name'    => 'WPAdCenter',
+					'module_type'     => 'plugin',
+					'version'         => WPADCENTER_VERSION,
+					'plugin_basename' => 'wpadcenter/wpadcenter.php',
+					'plugin_url'      => WPADCENTER_PLUGIN_URL,
+				)
+			);
+		}
 
-        return $adc_fs;
-    }
+		return $adc_fs;
+	}
 
-    // Init Analytics.
-    adc_fs();
-    // SDK initiated.
-    do_action('adc_fs_loaded');
+	// Init Analytics.
+	adc_fs();
+	// SDK initiated.
+	do_action( 'adc_fs_loaded' );
 }
 
-if (! defined('WPADCENTER_PLUGIN_FILENAME') ) {
-    define('WPADCENTER_PLUGIN_FILENAME', __FILE__);
+if ( ! defined( 'WPADCENTER_PLUGIN_FILENAME' ) ) {
+	define( 'WPADCENTER_PLUGIN_FILENAME', __FILE__ );
 }
 
-if (! defined('WPADCENTER_PLUGIN_BASENAME') ) {
-    define('WPADCENTER_PLUGIN_BASENAME', plugin_basename(WPADCENTER_PLUGIN_FILENAME));
+if ( ! defined( 'WPADCENTER_PLUGIN_BASENAME' ) ) {
+	define( 'WPADCENTER_PLUGIN_BASENAME', plugin_basename( WPADCENTER_PLUGIN_FILENAME ) );
 }
 
-if (! defined('WPADCENTER_SCRIPT_SUFFIX') ) {
-    define('WPADCENTER_SCRIPT_SUFFIX', ( defined('SCRIPT_DEBUG') && SCRIPT_DEBUG ) ? '' : '.min');
+if ( ! defined( 'WPADCENTER_SCRIPT_SUFFIX' ) ) {
+	define( 'WPADCENTER_SCRIPT_SUFFIX', ( defined( 'SCRIPT_DEBUG' ) && SCRIPT_DEBUG ) ? '' : '.min' );
 }
 
 
@@ -104,10 +103,9 @@ if (! defined('WPADCENTER_SCRIPT_SUFFIX') ) {
  * The code that runs during plugin activation.
  *  * This action is documented in includes/class-wpadcenter-activator.php
  */
-function activate_wpadcenter()
-{
-    include_once plugin_dir_path(__FILE__) . 'includes/class-wpadcenter-activator.php';
-    Wpadcenter_Activator::activate();
+function activate_wpadcenter() {
+	include_once plugin_dir_path( __FILE__ ) . 'includes/class-wpadcenter-activator.php';
+	Wpadcenter_Activator::activate();
 }
 
 
@@ -115,20 +113,19 @@ function activate_wpadcenter()
  * The code that runs during plugin deactivation.
  * This action is documented in includes/class-wpadcenter-deactivator.php
  */
-function deactivate_wpadcenter()
-{
-    include_once plugin_dir_path(__FILE__) . 'includes/class-wpadcenter-deactivator.php';
-    Wpadcenter_Deactivator::deactivate();
+function deactivate_wpadcenter() {
+	include_once plugin_dir_path( __FILE__ ) . 'includes/class-wpadcenter-deactivator.php';
+	Wpadcenter_Deactivator::deactivate();
 }
 
-register_activation_hook(__FILE__, 'activate_wpadcenter');
-register_deactivation_hook(__FILE__, 'deactivate_wpadcenter');
+register_activation_hook( __FILE__, 'activate_wpadcenter' );
+register_deactivation_hook( __FILE__, 'deactivate_wpadcenter' );
 
 /**
  * The core plugin class that is used to define internationalization,
  * admin-specific hooks, and public-facing site hooks.
  */
-require plugin_dir_path(__FILE__) . 'includes/class-wpadcenter.php';
+require plugin_dir_path( __FILE__ ) . 'includes/class-wpadcenter.php';
 
 /**
  * Begins execution of the plugin.
@@ -139,11 +136,9 @@ require plugin_dir_path(__FILE__) . 'includes/class-wpadcenter.php';
  *
  * @since 1.0.0
  */
-function run_wpadcenter()
-{
-
-    $plugin = new Wpadcenter();
-    $plugin->run();
+function run_wpadcenter() {
+	$plugin = new Wpadcenter();
+	$plugin->run();
 
 }
 run_wpadcenter();
@@ -155,10 +150,9 @@ run_wpadcenter();
  *
  * @return void echos html to render on frontend
  */
-function wpadcenter_display_ad( $atts )
-{
-    $shortcode = '[wpadcenter_ad id=' . $atts['id'] . ' align=' . $atts['align'] . ']';
-    echo do_shortcode($shortcode);
+function wpadcenter_display_ad( $atts ) {
+	$shortcode = '[wpadcenter_ad id=' . $atts['id'] . ' align=' . $atts['align'] . ']';
+	echo do_shortcode( $shortcode );
 }
 
 /**
@@ -168,8 +162,7 @@ function wpadcenter_display_ad( $atts )
  *
  * @return void echos html to render on frontend
  */
-function wpadcenter_display_adgroup( $atts )
-{
-    $shortcode = '[wpadcenter_adgroup adgroup_ids=' . $atts['adgroup_ids'] . ' align=' . $atts['align'] . ' num_ads=' . $atts['num_ads'] . ' num_columns=' . $atts['num_columns'] . ']';
-    echo do_shortcode($shortcode);
+function wpadcenter_display_adgroup( $atts ) {
+	$shortcode = '[wpadcenter_adgroup adgroup_ids=' . $atts['adgroup_ids'] . ' align=' . $atts['align'] . ' num_ads=' . $atts['num_ads'] . ' num_columns=' . $atts['num_columns'] . ']';
+	echo do_shortcode( $shortcode );
 }
