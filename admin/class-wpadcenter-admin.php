b01ee148d4ef0e1b80a8c768146e863307091626
dhananjay <dhananjay@hbwsls-MacBook-Air.local>
2021-04-20 13:19:38 +0530

    Added geo targeting feature.

--- admin/class-wpadcenter-admin.php
+++ admin/class-wpadcenter-admin.php
@@ -731,7 +731,7 @@ class Wpadcenter_Admin {
 					}
 				}
 				$the_options = apply_filters( 'wpadcenter_after_save_settings', $the_options );
-                update_option( WPADCENTER_SETTINGS_FIELD, $the_options );
+				update_option( WPADCENTER_SETTINGS_FIELD, $the_options );
 				echo '<div class="updated"><p><strong>' . esc_attr__(
 					'Settings Updated.',
 					'wpadcenter'
@@ -1700,6 +1700,21 @@ class Wpadcenter_Admin {
 
 		$current_ad_type = get_post_meta( $post->ID, 'wpadcenter_ad_type', true );
 		$current_ad_type = ! empty( $current_ad_type ) ? $current_ad_type : 'banner_image';
+
+		wp_enqueue_style(
+			$this->plugin_name . '-select2',
+			WPADCENTER_PLUGIN_URL . 'vendor/select2/select2/dist/css/select2.min.css',
+			array(),
+			$this->version,
+			'all'
+		);
+		wp_enqueue_script(
+			$this->plugin_name . '-select2',
+			WPADCENTER_PLUGIN_URL . 'vendor/select2/select2/dist/js/select2.min.js',
+			array( 'jquery' ),
+			$this->version,
+			false
+		);
 
 		wp_localize_script( $this->plugin_name, 'wpadcenter_render_metaboxes', array( $ad_meta_relation, $current_ad_type ) );
 		wp_enqueue_script( $this->plugin_name );
