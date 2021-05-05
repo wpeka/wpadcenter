6b948c57e85890bd02debf8e1dde0549f52ef6ae
Prem Surana <77276337+premsurana-hbwsl@users.noreply.github.com>
2021-05-05 15:45:08 +0530

    AD-111 + conflicts resolve (#173)
    Co-authored-by: premsurana <premx81@gmail.com>

--- includes/class-wpadcenter.php
+++ includes/class-wpadcenter.php
@@ -352,6 +352,7 @@ class Wpadcenter {
 			'hide_ads_logged'           => false,
 			'roles_selected'            => '',
 			'roles_selected_visibility' => '',
+			'content_ads'               => false,
 		);
 		$settings = apply_filters( 'wpadcenter_default_settings', $settings );
 		return '' !== $key ? $settings[ $key ] : $settings;
