99cd92f04ca46850ecc31fb77d080c4672db88b9
Pranay <pranay.chahare@hbwsl.com>
2021-07-30 11:26:11 +0530

    Fixed test case

--- tests/test-class-wpadcenter-admin.php
+++ tests/test-class-wpadcenter-admin.php
@@ -189,15 +189,19 @@ class Wpadcenter_Admin_Test extends WP_UnitTestCase {
 	public function test_wpadcenter_register_widgets() {
 		global $wp_version;
 
-		if ( version_compare( $wp_version, '5.8' ) >= 0 ) {
-			return;
-		}
 		self::$wpadcenter_admin->wpadcenter_register_widgets();
 		$widgets = array_keys( $GLOBALS['wp_widget_factory']->widgets );
 
-		$this->assertTrue( in_array( 'Wpadcenter_Single_Ad_Widget', $widgets, true ) );
-		$this->assertTrue( in_array( 'Wpadcenter_Adgroup_Widget', $widgets, true ) );
-		$this->assertTrue( in_array( 'Wpadcenter_Random_Ad_Widget', $widgets, true ) );
+		if ( version_compare( $wp_version, '5.8' ) >= 0 ) {
+			$this->assertFalse( in_array( 'Wpadcenter_Single_Ad_Widget', $widgets, true ) );
+			$this->assertFalse( in_array( 'Wpadcenter_Adgroup_Widget', $widgets, true ) );
+			$this->assertFalse( in_array( 'Wpadcenter_Random_Ad_Widget', $widgets, true ) );
+		} else {
+			$this->assertTrue( in_array( 'Wpadcenter_Single_Ad_Widget', $widgets, true ) );
+			$this->assertTrue( in_array( 'Wpadcenter_Adgroup_Widget', $widgets, true ) );
+			$this->assertTrue( in_array( 'Wpadcenter_Random_Ad_Widget', $widgets, true ) );
+
+		}
 
 	}
 
