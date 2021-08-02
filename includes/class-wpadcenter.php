4edeeff8f7ad1919005bbf236c2187b2c559e8a8
dhananjay <dhananjay@hbwsls-MacBook-Air.local>
2021-08-02 12:21:39 +0530

    Updated readme and plugin version number.

--- includes/class-wpadcenter.php
+++ includes/class-wpadcenter.php
@@ -80,7 +80,7 @@ class Wpadcenter {
 		if ( defined( 'WPADCENTER_VERSION' ) ) {
 			$this->version = WPADCENTER_VERSION;
 		} else {
-			$this->version = '2.2.2';
+			$this->version = '2.2.3';
 		}
 		$this->plugin_name = 'wpadcenter';
 
