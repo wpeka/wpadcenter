44870a9f2a7bb937f966cf87e9149a101ec712ea
Pranay <pranay.chahare@hbwsl.com>
2021-09-30 18:11:38 +0530

    js unit tests for contentads file

---
+++ jest.config.js
@@ -0,0 +1,18 @@
+module.exports = {
+	testPathIgnorePatterns: ['/node_modules/'],
+	moduleNameMapper: {
+		".+\\.(css|styl|less|sass|scss)$": "identity-obj-proxy",
+		".+\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/__mocks__/fileMock.js"
+	},
+	setupFilesAfterEnv: ['<rootDir>/jest-setup.js'],
+	globals: {
+		window: {},
+		localized_data : {
+				wpadcenter_pro_version : '5.2.3'
+			},
+		pro_ver_above_or_5_2_3 : true,
+		
+	},
+	"automock": false,
+  	"resetMocks": false,
+}
