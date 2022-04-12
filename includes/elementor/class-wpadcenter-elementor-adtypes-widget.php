8db8edfd4cd3976a9a6682a309dec017f5628ee4
Smriti Chaturvedi <smriti.chaturvedi@hbwsl.com>
2022-04-12 16:23:01 +0530

    Added Filters to display html for elementor ad widgets

--- includes/elementor/class-wpadcenter-elementor-adtypes-widget.php
+++ includes/elementor/class-wpadcenter-elementor-adtypes-widget.php
@@ -435,7 +435,7 @@ class Wpadcenter_Elementor_AdTypes_Widget extends \Elementor\Widget_Base {
 				'max_width_px' => $settings['max_width_px'],
 				'devices'      => $settings['devices'],
 			);
-			echo Wpadcenter_Pro_Public::display_ordered_ads( $ordered_ads_atts );// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo apply_filters( 'wpadcenter_display_elementor_ads', $ad_type, $ordered_ads_atts );// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 		} elseif ( 'animatedad' === $ad_type ) {
 			$ad_ids = array();
 			foreach ( $settings['list'] as $ad ) {
@@ -450,7 +450,7 @@ class Wpadcenter_Elementor_AdTypes_Widget extends \Elementor\Widget_Base {
 				'display_type' => $settings['display_type'],
 			);
 
-			echo Wpadcenter_Pro_Public::display_animated_ads( $animated_ads_atts );// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo apply_filters( 'wpadcenter_display_elementor_ads', $ad_type, $animated_ads_atts );// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 		} elseif ( 'rotatingad' === $ad_type ) {
 			$adgroup_id = $settings['adgroup_id'];
@@ -462,7 +462,7 @@ class Wpadcenter_Elementor_AdTypes_Widget extends \Elementor\Widget_Base {
 				'max_width_px' => $settings['max_width_px'],
 				'devices'      => $settings['devices'],
 			);
-			echo Wpadcenter_Pro_Public::display_rotating_adgroup( $adgroup_id, $attributes );// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
+			echo apply_filters( 'wpadcenter_display_elementor_ads', $ad_type, $attributes, $adgroup_id );// phpcs:ignore WordPress.Security.EscapeOutput.OutputNotEscaped
 
 		}
 	}
