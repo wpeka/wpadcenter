41660c90c195e731d7bbf875df8639d1dfce9db2
Anusha913082 <anusha.raju@hbwsl.com>
2022-04-22 16:13:06 +0530

    AD-222 Modified the notice and added transient to make it appear after 1 day

--- admin/class-wpadcenter-admin.php
+++ admin/class-wpadcenter-admin.php
@@ -294,17 +294,40 @@ class Wpadcenter_Admin {
 	 * @since 5.4.0
 	 */
 	public function wpadcenter_blocks_widgets_deprecation_notice() {
-		$current_screen = get_current_screen();
-		if ( 'edit-wpadcenter-ads' === $current_screen->id || 'wpadcenter-ads' === $current_screen->id || 'edit-wpadcenter-adgroups' === $current_screen->id ) {
-			?>
-			<div id="wpadcenter-blocks-widgets-deprecation-warning">
-				<p>
-					<?php
-					printf( 'We have added consolidated single Ad blocks in Gutenberg (WPAdCenter Ad block), and Elementor (WPAdCenter Ad Widget) for better user experience.  Kindly replace your existing blocks/widgets with the new one, as the old ones will get deprecated soon.' );
-					?>
-				</p>
-			</div>
-			<?php
+		$current_screen                  = get_current_screen();
+		$check_for_deprecation_transient = get_transient( 'wpadcenter_deprecation_transient' );
+
+		if ( ! $check_for_deprecation_transient ) {
+			if ( 'edit-wpadcenter-ads' === $current_screen->id || 'wpadcenter-ads' === $current_screen->id || 'edit-wpadcenter-adgroups' === $current_screen->id ) {
+				?>
+				<form method="post">
+					<div id="wpadcenter-blocks-widgets-deprecation-warning">
+						<p>
+							<?php
+							printf( 'Please replace your existing ad widgets/blocks with the new one - WPAdCenter Ad Block (Gutenberg) & WPAdCenter Ad Widget (Elementor), as the old ones will be deprecated soon. <a href="' . esc_url( 'https://docs.wpeka.com/wp-adcenter/placing-ads/placing-ad-using-consolidated-block-elementor-widget' ) . '" target="_blank" rel="noopener noreferrer"> Learn more</a>.' );
+							?>
+						</p>
+						<button class="vvv" style="border: none;padding:0;background: none;color: #2271b1;"><i class="dashicons dashicons-dismiss"></i></button>
+						<input type="hidden" id="wpadcenter_deprecation_nonce" name="wpadcenter_deprecation_nonce" value="<?php echo esc_attr( wp_create_nonce( 'wpadcenter_deprecation' ) ); ?>" />
+					</div>
+				</form>
+				<?php
+			}
+		}
+	}
+
+	/**
+	 * Set transient for deprecation notice.
+	 *
+	 * @since 5.4.0
+	 */
+	public function wpadcenter_deprecation_already_done() {
+		$dnd = '';
+		if ( isset( $_POST['wpadcenter_deprecation_nonce'] ) && check_admin_referer( 'wpadcenter_deprecation', 'wpadcenter_deprecation_nonce' ) ) {
+			$check_for_deprecation_transient = get_transient( 'wpadcenter_deprecation_transient' );
+			if ( false === $check_for_deprecation_transient ) {
+				set_transient( 'wpadcenter_deprecation_transient', 'Deprecation Pending', 86400 );
+			}
 		}
 	}
 
