a9931137ddda3007c63cef7097b4515efd3448a3
RAJAT <rajat.gangwani@hbwsl.com>
2023-08-08 13:20:33 +0530

    Added change log for 2.5.4 release for columns issue fix.

--- includes/class-wpadcenter.php
+++ includes/class-wpadcenter.php
@@ -80,7 +80,7 @@ class Wpadcenter {
 		if ( defined( 'WPADCENTER_VERSION' ) ) {
 			$this->version = WPADCENTER_VERSION;
 		} else {
-			$this->version = '2.5.3';
+			$this->version = '2.5.4';
 		}
 		$this->plugin_name = 'wpadcenter';
 
