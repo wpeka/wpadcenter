8e61ee5e1f6fa13d25dd3d918221a746b2ac1ea8
OjasA21 <ojas.awachat@hbwsl.com>
2025-04-28 14:37:08 +0530

    Added version number and change log

--- includes/class-wpadcenter.php
+++ includes/class-wpadcenter.php
@@ -80,7 +80,7 @@ class Wpadcenter {
 		if ( defined( 'WPADCENTER_VERSION' ) ) {
 			$this->version = WPADCENTER_VERSION;
 		} else {
-			$this->version = '2.5.9';
+			$this->version = '2.6.0';
 		}
 		$this->plugin_name = 'wpadcenter';
 
